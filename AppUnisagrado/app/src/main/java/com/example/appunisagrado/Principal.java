08babfa5d5fe57e803d33c475659d4aec89414df
LipeGabriell <felipe.ramiressanches@hotmail.com>
2022-04-25 04:16:37 -0300

    25/4

--- AppUnisagrado/app/src/main/java/com/example/appunisagrado/Principal.java
+++ AppUnisagrado/app/src/main/java/com/example/appunisagrado/Principal.java
@@ -1,6 +1,7 @@
 package com.example.appunisagrado;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,8 @@ public class Principal extends AppCompatActivity {
 
         Button informacoesButton = (Button) findViewById(R.id.buttonInfos);
         Button noticiasButton = (Button) findViewById(R.id.buttonNoticias);
+        Button ajudaButton = (Button) findViewById(R.id.buttonAjuda);
+        Button mapaButton = (Button) findViewById(R.id.buttonMapa);
 
             informacoesButton.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -32,6 +35,25 @@ public class Principal extends AppCompatActivity {
                     startActivity(new Intent(Principal.this, dados.class));;
                 }
             });
+
+            ajudaButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("tel:136"));
+                    startActivity(callIntent);
+
+                }
+            });
+
+            mapaButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(Principal.this, mapa.class));
+                }
+            });
+
+
 
     }
 
